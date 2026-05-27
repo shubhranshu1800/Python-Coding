72fb8374703d49ec8187090a5997505a188b83e4
shubhranshu1800 <shubhranshu1800@gmail.com>
2026-05-27 06:11:19 -0500

    Add files via upload

---
+++ Calculator using python.py
@@ -0,0 +1,18 @@
+a = 3
+b = 2
+value1 = a+b
+print("addition of the", a, "and", b, "is", value1)
+value2= a-b
+print("subtraction of the", a, "and", b, "is", value2)
+value3= a*b
+print("multiplication of the", a, "and", b, "is", value3)
+value4=a/b
+print("division of the", a, "and", b, "is", value4)
+value5=a//b
+print("Floor division  of the", a, "and", b, "is", value5)
+value6= a**b
+print("exponentiation of the", a, "and", b, "is", value6)
+value7=a%b
+print("modulo the", a, "and", b, "is", value7)
+
+
